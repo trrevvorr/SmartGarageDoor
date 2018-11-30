16fed1dd59200be1626c25c4ec1612f0316ea9ad
trrevvorr <t2r1Github>
2018-11-29 23:20:20 -0600

    use single redirect query string param instead

--- authorize/authorizeInstagram.js
+++ authorize/authorizeInstagram.js
@@ -1,12 +1,12 @@
 // assuming url = https://trrevvorr.github.io/InternetPoints/authorize/authorizeInstagram?particle_access_token=<TOKEN>&particle_device_id=<TOKEN>#access_token=ACCESS-TOKEN
 
 const urlParams = new URLSearchParams(window.location.search);
-const particleAccessToken = urlParams.get("particle_access_token");
-const particleDeviceId = urlParams.get("particle_device_id");
+const particleCredentials = urlParams.get("particle_credentials");
+const { particleAccessToken, particleDeviceId } = decodeParticleCredentials(particleCredentials);
 const instagramAccessToken = getAccessToken();
 console.log([particleAccessToken, particleDeviceId, instagramAccessToken])
 const particleRequest = `https://api.particle.io/v1/devices/${particleDeviceId}/authorizeInstagram?access_token=${particleAccessToken}&arg=${instagramAccessToken}`;
-document.setTimeout(successMessage, 100); // give DOM time to render
+setTimeout(successMessage, 100); // give DOM time to render
 
 
 function getAccessToken() {
@@ -26,4 +26,13 @@ function getAccessToken() {
 function successMessage() {
 	const statusNode = document.querySelector("#status");
 	statusNode.textContent = "Instagram Account Linked!"
+}
+
+function decodeParticleCredentials(particleCredentials) {
+	// see linkAccount.js for encoding
+	const delimeter = "-";
+	const delimeterIndex = particleCredentials.indexOf(delimeter);
+	const particleAccessToken = particleCredentials.substring(0, delimeterIndex);
+	const particleDeviceId = particleCredentials.substring(delimeterIndex + 1);
+	return { particleAccessToken, particleDeviceId };
 }
--- link/linkAccount.js
+++ link/linkAccount.js
@@ -4,9 +4,16 @@ function linkInstagram() {
 	const urlParams = new URLSearchParams(window.location.search);
 	const particleAccessToken = urlParams.get("particle_access_token");
 	const particleDeviceId = urlParams.get("particle_device_id");
+	const particleCredentials = encodeParticleCredentials(particleAccessToken, particleDeviceId);
 	const instagramClientId = urlParams.get("instagram_client_id");
-	const redirectURI = encodeURI(`https://trrevvorr.github.io/InternetPoints/authorize/authorizeInstagram?particle_access_token=${particleAccessToken}&particle_device_id=${particleDeviceId}`);
+	const redirectURI = encodeURI(`https://trrevvorr.github.io/InternetPoints/authorize/authorizeInstagram?particle_credentials=${particleCredentials}`);
 	const instagramRequest = `https://api.instagram.com/oauth/authorize/?client_id=${instagramClientId}&response_type=token&redirect_uri=${redirectURI}`;
 
 	window.location.href = instagramRequest;
+}
+
+function encodeParticleCredentials(particleAccessToken, particleDeviceId) {
+	// see authorizeInstagram.js for decoding
+	const delimeter = "-";
+	return particleAccessToken + delimeter + particleDeviceId;
 }
