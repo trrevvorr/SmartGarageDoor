0dc46ef10cdb4c1f08baa86ad853eed6a10f8832
Trevor Ross <trevor.is.ross@gmail.com>
2018-12-01 13:52:49 -0500

    send instagram access token down to particle

--- authorize/authorizeInstagram.js
+++ authorize/authorizeInstagram.js
@@ -4,9 +4,12 @@ const urlParams = new URLSearchParams(window.location.search);
 const particleCredentials = urlParams.get("particle_credentials");
 const { particleAccessToken, particleDeviceId } = decodeParticleCredentials(particleCredentials);
 const instagramAccessToken = getAccessToken();
-console.log([particleAccessToken, particleDeviceId, instagramAccessToken])
-const particleRequest = `https://api.particle.io/v1/devices/${particleDeviceId}/authorizeInstagram?access_token=${particleAccessToken}&arg=${instagramAccessToken}`;
-setTimeout(successMessage, 100); // give DOM time to render
+if (validateInstagramResponse(instagramAccessToken)) {
+	sendInstagramTokenToParticle(particleAccessToken, particleDeviceId, instagramAccessToken);
+} else {
+	console.log([particleAccessToken, particleDeviceId, instagramAccessToken]);
+	failMessage();
+}
 
 
 function getAccessToken() {
@@ -24,8 +27,17 @@ function getAccessToken() {
 }
 
 function successMessage() {
+	setStatusMessage("Instagram Account Linked!");
+}
+
+
+function failMessage() {
+	setStatusMessage("Failed to link Instagram account. Please try again later.");
+}
+
+function setStatusMessage(status) {
 	const statusNode = document.querySelector("#status");
-	statusNode.textContent = "Instagram Account Linked!"
+	statusNode.textContent = status;
 }
 
 function decodeParticleCredentials(particleCredentials) {
@@ -35,4 +47,17 @@ function decodeParticleCredentials(particleCredentials) {
 	const particleAccessToken = particleCredentials.substring(0, delimeterIndex);
 	const particleDeviceId = particleCredentials.substring(delimeterIndex + 1);
 	return { particleAccessToken, particleDeviceId };
+}
+
+function validateInstagramResponse(instagramAccessToken) {
+	return instagramAccessToken.length > 0;
+}
+
+function sendInstagramTokenToParticle(particleAccessToken, particleDeviceId, instagramAccessToken) {
+	const url = `https://api.particle.io/v1/devices/${particleDeviceId}/linkIG`;
+	const data = {
+		access_token = particleAccessToken,
+		arg = instagramAccessToken,
+	};
+	var postRequest = $.post( url, data).done(successMessage()).fail(failMessage());
 }
